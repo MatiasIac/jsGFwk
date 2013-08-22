b3af9538780565a38fe20b0fffd578a1fdbb3028
Matias Iacono <matias@kinetica-solutions.com>
2013-08-22 20:08:16 -0300

    Mouse handling added

--- Framework/jsGFwkIO.js
+++ Framework/jsGFwkIO.js
@@ -17,11 +17,53 @@ jsGFwk.IO = {
 	},
 	
 	mouse: {
+		_mouseClickCallers: [],
+		_mouseMoveCallers: [],
+		
+		_lastDownCoords: {},
+		_lastMoveCoords: {},
+		_isMousePressed: false,
+		
+		_getCoordinates: function(e) {
+			if (e.offsetX)
+				return { x: e.offsetX, y: e.offsetY };
+			else if (e.layerX)
+				return { x: e.layerX, y: e.layerY };
+			else
+				return { x: e.pageX - jsGFwk._canvas.offsetLeft, y: e.pageY - jsGFwk._canvas.offsetTop };
+		},
+		
+		_mouseDown: function(e) {
+			jsGFwk.IO.mouse._isMousePressed = true;
+			jsGFwk.IO.mouse._lastDownCoords = jsGFwk.IO.mouse._getCoordinates(e);
+		},
+		
+		_mouseUp: function() {
+			jsGFwk.IO.mouse._isMousePressed = false;
+			for (var i = 0; i < jsGFwk.IO.mouse._mouseClickCallers.length; jsGFwk.IO.mouse._mouseClickCallers[i++](jsGFwk.IO.mouse._lastDownCoords));
+		},
+		
+		_mouseMove: function(e) {
+			jsGFwk.IO.mouse._lastMoveCoords = jsGFwk.IO.mouse._getCoordinates(e);
+			for (var i = 0; i < jsGFwk.IO.mouse._mouseMoveCallers.length; jsGFwk.IO.mouse._mouseMoveCallers[i++](jsGFwk.IO.mouse._lastMoveCoords));
+		},
+		
+		registerClick: function(f) {
+			this._mouseClickCallers.push(f);
+		},
+		
+		registerMove: function(f) {
+			this._mouseMoveCallers.push(f);
+		}
 	},
 	
 	start: function() {
 		//Register all listeners
 		document.addEventListener("keydown", this.keyboard._keyPressed, false);
 		document.addEventListener("keyup", this.keyboard._keyReleased, false);
+		
+		document.getElementById(jsGFwk.settings.canvas).addEventListener("mousedown", this.mouse._mouseDown, false);
+		document.getElementById(jsGFwk.settings.canvas).addEventListener("mouseup", this.mouse._mouseUp, false);
+		document.getElementById(jsGFwk.settings.canvas).addEventListener("mousemove", this.mouse._mouseMove, false);
 	}
 };
