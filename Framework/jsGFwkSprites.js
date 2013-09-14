196a66e50cc5614a7126bafa7a8e93e86cc23621
Matias Iacono <matias@kinetica-solutions.com>
2013-09-14 11:39:10 -0300

    Corrected Galaga sample. Added plug in to crop sprite sheets

---
+++ Framework/jsGFwkSprites.js
@@ -0,0 +1,28 @@
+jsGFwk.Sprites = {
+
+	sprites: {},
+	
+	//{ id: 'Sprite Name', graphic: image, top: 0, left: 0, width: 0, height: 0 }
+	createSprite: function (spriteObject) {
+		var tempCanvas = document.createElement("canvas");
+		tempCanvas.width = spriteObject.width;
+		tempCanvas.height = spriteObject.height;
+		var tempContext = tempCanvas.getContext("2d");
+		
+		tempContext.drawImage(spriteObject.graphic, 
+			spriteObject.top, spriteObject.left, spriteObject.width, spriteObject.height,
+			0,0,spriteObject.width, spriteObject.height);
+		
+		jsGFwk.Sprites.sprites[spriteObject.id] = {};
+		jsGFwk.Sprites.sprites[spriteObject.id].top = spriteObject.top;
+		jsGFwk.Sprites.sprites[spriteObject.id].left = spriteObject.left;
+		jsGFwk.Sprites.sprites[spriteObject.id].width = spriteObject.width;
+		jsGFwk.Sprites.sprites[spriteObject.id].height = spriteObject.height;
+		jsGFwk.Sprites.sprites[spriteObject.id].image = new Image();
+		jsGFwk.Sprites.sprites[spriteObject.id].image.src = tempCanvas.toDataURL();
+	},
+
+	start: function () {
+		
+	}
+};
--- Readme.txt
+++ Readme.txt
@@ -3,5 +3,12 @@ This is a simple minimalistic game framework for JavaScript and HTML5
 This framework will allow you to:
 
 - Create simple 2D games with HTML5 and JavaScript
-- Handle image and sound resources for your games
-- Be used as a foundation for complex games or to work with other frameworks
+- Handles image and sound resources for your games
+- Includes basic rect boxing collision
+- Handles keyboard and mouse for your games
+
+More important:
+
+- To be used as a foundation for complex games or to work with other frameworks
+
+AND REMEMBER. SIMPLICITY IS THE BEST.
