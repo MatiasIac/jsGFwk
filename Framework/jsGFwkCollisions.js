f83cab7128d0a963b0b4beeb3f4699d5e1aea034
Matias Iacono <matias@kinetica-solutions.com>
2013-09-15 18:40:35 -0300

    Radius collision functionality

--- Framework/jsGFwkCollisions.js
+++ Framework/jsGFwkCollisions.js
@@ -17,12 +17,27 @@ jsGFwk.Collisions = {
         return true;
 	},
 
+	_disColliding: function (otherObject) {
+		if (!otherObject) { return false; }
+		if (!this.radius || !this.x || !this.y) { return false; }
+		if (!otherObject.radius || !otherObject.x || !otherObject.y) { return false; }
+		
+		var dx = otherObject.x - this.x;
+        var dy = otherObject.y - this.y;
+        var dist = Math.sqrt(dx * dx + dy * dy);
+
+        return (dist < this.radius + otherObject.radius);
+	},
+	
 	onStart: function () {
-		Object.prototype.isRectColliding = this._rectColliding;
+		
 	},
 	
 	onObjectCreated: function (newObject) {
 		if (!newObject.width) { newObject.width = 1; }
 		if (!newObject.height) { newObject.height = 1; }
+		if (!newObject.radius) { newObject.radius = 1; }
+		newObject.isRectColliding = this._rectColliding;
+		newObject.isRadColliding = this._disColliding;
 	}
 };
