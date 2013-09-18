116c61e9e01bbec04f9812df3d3b312e256d548b
Matias Iacono <matias@kinetica-solutions.com>
2013-09-17 23:36:09 -0300

    Sprite collections

--- Framework/jsGFwkSprites.js
+++ Framework/jsGFwkSprites.js
@@ -1,25 +1,67 @@
 jsGFwk.Sprites = {
 
-	sprites: {},
-	
-	//{ id: 'Sprite Name', graphic: image, top: 0, left: 0, width: 0, height: 0 }
-	createSprite: function (spriteObject) {
+	_genSprite: function (spriteObject) {
 		var tempCanvas = document.createElement("canvas");
 		tempCanvas.width = spriteObject.width;
 		tempCanvas.height = spriteObject.height;
 		var tempContext = tempCanvas.getContext("2d");
 		
 		tempContext.drawImage(spriteObject.graphic, 
-			spriteObject.top, spriteObject.left, spriteObject.width, spriteObject.height,
+			spriteObject.left, spriteObject.top, spriteObject.width, spriteObject.height,
 			0,0,spriteObject.width, spriteObject.height);
+			
+		return tempCanvas.toDataURL();
+	},
+	
+	createSpriteCollection: function (collectionId, graphic, values) {
+		jsGFwk.Sprites[collectionId] = {
+			_looper: function() {
+				this.seeker++;
+				this.seeker = this.seeker % this.spriteBag.length;
+			},
+			_oneWay: function() {
+				if (this.seeker < this.spriteBag.length) {	
+					this.seeker++;
+				}
+			},
+			_moveFrame: function() {},
+			spriteBag: [],
+			seeker: -1,
+			loop: function(on) {
+				if (on) {
+					this._moveFrame = this._looper;
+				} else {
+					this._moveFrame = this._oneWay;
+				}
+			},
+			next: function () {
+				this._moveFrame();				
+				this.sprite = this.spriteBag[this.seeker];
+			},
+			sprite: {},
+		};
 		
-		jsGFwk.Sprites.sprites[spriteObject.id] = {};
-		jsGFwk.Sprites.sprites[spriteObject.id].top = spriteObject.top;
-		jsGFwk.Sprites.sprites[spriteObject.id].left = spriteObject.left;
-		jsGFwk.Sprites.sprites[spriteObject.id].width = spriteObject.width;
-		jsGFwk.Sprites.sprites[spriteObject.id].height = spriteObject.height;
-		jsGFwk.Sprites.sprites[spriteObject.id].image = new Image();
-		jsGFwk.Sprites.sprites[spriteObject.id].image.src = tempCanvas.toDataURL();
+		for (var i = 0; i < values.length; i++) {
+			var spriteObject = { top: values[i].top, left: values[i].left, width: values[i].width, height: values[i].height, graphic: graphic };
+			var image = new Image();
+			image.src = this._genSprite(spriteObject);
+			spriteObject.image = image;
+			delete spriteObject.graphic;
+			
+			jsGFwk.Sprites[collectionId].spriteBag[i] = spriteObject;
+			jsGFwk.Sprites[collectionId].loop(false);
+		}
+	},
+	
+	//{ id: 'Sprite Name', graphic: image, top: 0, left: 0, width: 0, height: 0 }
+	createSprite: function (spriteObject) {
+		jsGFwk.Sprites[spriteObject.id] = {};
+		jsGFwk.Sprites[spriteObject.id].top = spriteObject.top;
+		jsGFwk.Sprites[spriteObject.id].left = spriteObject.left;
+		jsGFwk.Sprites[spriteObject.id].width = spriteObject.width;
+		jsGFwk.Sprites[spriteObject.id].height = spriteObject.height;
+		jsGFwk.Sprites[spriteObject.id].image = new Image();
+		jsGFwk.Sprites[spriteObject.id].image.src = this._genSprite(spriteObject);
 	},
 
 	onStart: function () {
