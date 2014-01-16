f21273e39770153ac0045ab21150e7bd678a4d13
matiasiacono <matias@kinetica-solutions.com>
2014-01-16 17:58:19 -0300

    Added new function to just rotate an object and then execute a package of actions

--- Framework/jsGFwkEffects.js
+++ Framework/jsGFwkEffects.js
@@ -20,11 +20,30 @@ jsGFwk.Effects = {
 			context.drawImage(params.sprite.image, 0, 0);
 		context.restore();
 	},
+	
+	rotate: function (params) {
+		if (params == undefined || params == null) { return; }
+		if (params.then == undefined || params.then == null) { return; }
+		
+		if (!this.rotationPoint) { 
+			this.rotationPoint = { x: 0, y: 0 };
+		}
+	
+		var context = jsGFwk._2Dbuffer;
+		context.save();
+			context.translate(this.x, this.y);
+			context.rotate(jsGFwk.Effects.degreeToRadians(params.angle));
+			context.translate(-this.rotationPoint.x, -this.rotationPoint.y);
+		
+			params.then(context);
+		context.restore();
+	},
 
 	onStart: function () {
 	},
 	
 	onObjectCreated: function (newObject) {
 		newObject.rotateAndDraw = this.rotateAndDraw;
+		newObject.rotate = this.rotate;
 	}
 };
