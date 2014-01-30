784e85bdc93a7e74d8d626cf5e24b71ce8dc5043
matiasiacono <matias@kinetica-solutions.com>
2014-01-29 21:09:30 -0300

    Solve #3: Multiple sound sources

--- Framework/jsGFwkRM.js
+++ Framework/jsGFwkRM.js
@@ -11,7 +11,14 @@ jsGFwk.ResourceManager = {
 			}
 		},
 		
-		sounds: {},
+		sounds: {
+			format: { 
+				ogg: 'audio/ogg; codecs="vorbis"',
+				wav: 'audio/wav; codecs="1"',
+				mp3: 'audio/mpeg;',
+				acc: 'audio/mp4; codecs="mp4a.40.2"'
+			}
+		},
 		graphics: {},
 		
 		//source: { name: "object Name", source: "sprite Path", image: created by the framework }
@@ -26,10 +33,29 @@ jsGFwk.ResourceManager = {
 		//source: { name: "object Name", source: "audio Path", audio: created by the framework }
 		addSound: function (source) {
 			var sound = new Audio();
-			sound.addEventListener('canplaythrough', this._loadDispatcher, false);
-			source.audio = sound;
-			this.sounds[source.name] = source;
-			this._totalResources++;
+			var errorFound = false;
+			
+			//Many possible sources
+			if (source.sources != undefined) {
+				//Replace the property source.source with the correct source
+				for (var format in this.sounds.format) {
+					if (!!(sound.canPlayType && sound.canPlayType(this.sounds.format[format]).replace(/no/, ''))) {
+						if (source.sources[this.sounds.format[format]] == undefined) { 
+							errorFound = true;
+							break;
+						}
+						source.source = source.sources[this.sounds.format[format]].source;
+						break;
+					}
+				}
+			}
+			
+			if (!errorFound) {
+				sound.addEventListener('canplaythrough', this._loadDispatcher, false);
+				source.audio = sound;
+				this.sounds[source.name] = source;
+				this._totalResources++;
+			}
 		},
 		
 		onStart: function() {
@@ -41,7 +67,7 @@ jsGFwk.ResourceManager = {
 				}
 				
 				for (var soundSource in jsGFwk.ResourceManager.sounds) {
-					if (jsGFwk.ResourceManager.sounds.hasOwnProperty(soundSource)) {
+					if (jsGFwk.ResourceManager.sounds.hasOwnProperty(soundSource) && soundSource !== 'format') {
 						jsGFwk.ResourceManager.sounds[soundSource].audio.src = jsGFwk.ResourceManager.sounds[soundSource].source;
 					}
 				}
