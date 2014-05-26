7fbe6893dd41f27ea64efcaa965526d884c5c626
matiasiacono <matias@kinetica-solutions.com>
2014-05-26 14:53:33 -0300

    New DARKER filter

--- Framework/jsGFwkSprites.js
+++ Framework/jsGFwkSprites.js
@@ -102,6 +102,17 @@ jsGFwk.Sprites = {
 				data[i + 2] = 255 - data[i + 2];
 			}
 			context.putImageData(imageData, 0, 0);
+		},
+		DARKER: function (context, img) {
+		    var imageData = context.getImageData(0, 0, img.width, img.height);
+		    var data = imageData.data;
+		    for (var i = 0; i < data.length; i += 4) {
+                
+		        data[i] = (data[i] - 50 < 0 ? 0 : data[i] - 50);
+		        data[i + 1] = (data[i + 1] - 50 < 0 ? 0 : data[i + 1] - 50);
+		        data[i + 2] = (data[i + 2] - 50 < 0 ? 0 : data[i + 2] - 50);
+		    }
+		    context.putImageData(imageData, 0, 0);
 		}
 	},
 
