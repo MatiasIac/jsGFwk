a73aad41a70b4269018435b7cf8b6f89ba8d6137
matiasiacono <matias@kinetica-solutions.com>
2014-08-19 17:05:48 -0300

    More effects

--- Framework/jsGFwkSprites.js
+++ Framework/jsGFwkSprites.js
@@ -81,6 +81,101 @@ jsGFwk.Sprites = {
 	},
 		
 	filters: {
+		_convolute: function (context, weights, img) {
+			var side = Math.round(Math.sqrt(weights.length));
+			var halfSide = Math.floor(side / 2);
+			var pixels = context.getImageData(0, 0, img.width, img.height);
+			var src = pixels.data;
+			var sw = pixels.width;
+			var sh = pixels.height;
+			var w = pixels.width;
+			var h = pixels.height;
+			
+			var tempCanvas = document.createElement("canvas");
+			tempCanvas.width = w;
+			tempCanvas.height = h;
+			var tempContext = tempCanvas.getContext("2d");
+			var pixelData = tempContext.createImageData(w, h);
+			var dst = pixelData.data;
+			
+			var alphaFac = 0;
+			for (var y = 0; y < h; y++) {
+				for (var x = 0; x < w; x++) {
+					var sy = y;
+					var sx = x;
+					var dstOff = (y * w + x) * 4;
+					var r = 0, g = 0, b = 0, a = 0;
+					for (var cy = 0; cy < side; cy++) {
+						for (var cx = 0; cx < side; cx++) {
+							var scy = sy + cy - halfSide;
+							var scx = sx + cx - halfSide;
+							if (scy >= 0 && scy < sh && scx >= 0 && scx < sw) {
+								var srcOff = (scy * sw + scx) * 4;
+								var wt = weights[cy * side + cx];
+								r += src[srcOff] * wt;
+								g += src[srcOff+1] * wt;
+								b += src[srcOff+2] * wt;
+								a += src[srcOff+3] * wt;
+							}
+						}
+					}
+					dst[dstOff] = r;
+					dst[dstOff+1] = g;
+					dst[dstOff+2] = b;
+					dst[dstOff+3] = a + alphaFac * (255 - a);
+				}
+			}
+			
+			context.putImageData(pixelData, 0, 0);
+		},
+		_convolute2: function (context, weights, img, offset, divisor) {
+			if (!divisor) {
+				divisor = weights.reduce(function(a, b) {return a + b;}) || 1;
+			}
+			var pixels = context.getImageData(0, 0, img.width, img.height);
+			var src = pixels.data;
+			var sw = pixels.width;
+			var sh = pixels.height;
+			var w = pixels.width;
+			var h = pixels.height;
+			
+			var tempCanvas = document.createElement("canvas");
+			tempCanvas.width = w;
+			tempCanvas.height = h;
+			var tempContext = tempCanvas.getContext("2d");
+			var pixelData = tempContext.createImageData(w, h);
+			var dst = pixelData.data;
+			var len = dst.length; 
+			var res = 0;
+
+			for (var i = 0; i < len; i++) {
+				if ((i + 1) % 4 === 0) {
+				  dst[i] = src[i];
+				  continue;
+				}
+				res = 0;
+				var these = [
+				  src[i - w * 4 - 4] || src[i],
+				  src[i - w * 4]     || src[i],
+				  src[i - w * 4 + 4] || src[i],
+				  src[i - 4]         || src[i],
+				  src[i],
+				  src[i + 4]         || src[i],
+				  src[i + w * 4 - 4] || src[i],
+				  src[i + w * 4]     || src[i],
+				  src[i + w * 4 + 4] || src[i]
+				];
+				for (var j = 0; j < 9; j++) {
+				  res += these[j] * weights[j];
+				}
+				res /= divisor;
+				if (offset) {
+				  res += offset;
+				}
+				dst[i] = res;
+			}
+			context.putImageData(pixelData, 0, 0);
+		},
 		GRAYSCALE: function (context, img) {
 			var imageData = context.getImageData(0, 0, img.width, img.height);
 			var data = imageData.data;
@@ -116,53 +211,28 @@ jsGFwk.Sprites = {
 		},
 		BLUR: function (context, img) {
 			var weights = [1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9];
-			var side = Math.round(Math.sqrt(weights.length));
-			var halfSide = Math.floor(side / 2);
-			var pixels = context.getImageData(0, 0, img.width, img.height); //getPixelData(v, w, h);
-			var src = pixels.data;
-			var sw = pixels.width;
-			var sh = pixels.height;
-			var w = pixels.width;
-			var h = pixels.height;
-			
-			var tempCanvas = document.createElement("canvas");
-			tempCanvas.width = w;
-			tempCanvas.height = h;
-			var tempContext = tempCanvas.getContext("2d");
-			var pixelData = tempContext.createImageData(w, h);
-			var dst = pixelData.data;
-			
-			// Iterate through the destination image pixels
-			var alphaFac = 0;
-			for (var y = 0; y < h; y++) {
-				for (var x = 0; x < w; x++) {
-					var sy = y;
-					var sx = x;
-					var dstOff = (y * w + x) * 4;
-					var r = 0, g = 0, b = 0, a = 0;
-					for (var cy = 0; cy < side; cy++) {
-						for (var cx = 0; cx < side; cx++) {
-							var scy = sy + cy - halfSide;
-							var scx = sx + cx - halfSide;
-							if (scy >= 0 && scy < sh && scx >= 0 && scx < sw) {
-								var srcOff = (scy * sw + scx) * 4;
-								var wt = weights[cy * side + cx];
-								r += src[srcOff] * wt;
-								g += src[srcOff+1] * wt;
-								b += src[srcOff+2] * wt;
-								a += src[srcOff+3] * wt;
-							}
-						}
-					}
-					dst[dstOff] = r;
-					dst[dstOff+1] = g;
-					dst[dstOff+2] = b;
-					dst[dstOff+3] = a + alphaFac * (255 - a);
-				}
-			}
-			
-			context.putImageData(pixelData, 0, 0);
-		}
+			jsGFwk.Sprites.filters._convolute(context, weights, img);
+		},
+		SHARPEN: function (context, img) {
+			var weights = [ 0, -2, 0, -2, 11, -2, 0, -2, 0];
+			jsGFwk.Sprites.filters._convolute2(context, weights, img);
+		},
+		EMBOSSSUBTLE: function (context, img) {
+			var weights = [1, 1, -1, 1, 3, -1, 1, -1, -1];
+			jsGFwk.Sprites.filters._convolute2(context, weights, img);
+		},
+		EMBOSS: function (context, img) {
+			var weights = [2, 0, 0, 0, -1, 0, 0, 0, -1];
+			jsGFwk.Sprites.filters._convolute2(context, weights, img, 127);
+		},
+		EDGEDETECT: function (context, img) {
+			var weights = [1, 1, 1, 1, -7, 1, 1, 1, 1];
+			jsGFwk.Sprites.filters._convolute2(context, weights, img);
+		},
+		EDGEDETECT2: function (context, img) {
+			var weights = [-5, 0, 0, 0, 0, 0, 0, 0, 5];
+			jsGFwk.Sprites.filters._convolute2(context, weights, img);
+		}		
 	},
 
 	onStart: function () {
