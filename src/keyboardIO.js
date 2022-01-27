9950e022d74249433929381932735017a7aaaccd
Matias Iacono <matias.iacono@gmail.com>
2022-01-27 15:37:42 +1300

    Touch and Keyboard

---
+++ src/keyboardIO.js
@@ -0,0 +1,66 @@
+class KeyboardIO {
+
+    _name = "KeyboardIO";
+    _keyboardCallers = [];
+    _activeKey = [];
+
+    static KEYS = {
+        "A": 65,
+        "D": 68,
+        "I": 73,
+        "J": 74,
+        "K": 75,
+        "L": 76,
+        "N": 78,
+        "S": 83,
+        "W": 87,
+        "M": 77,
+        "N": 78,
+        "C": 67,
+        "ONE": 49,
+        "TWO": 50,
+        "THREE": 51,
+        "SHIFT": 16,
+        "SPACEBAR": 32,
+        "ENTER": 13,
+        "CONTROL": 17
+    };
+
+    constructor() { }
+
+    _stopPropagation() { }
+
+    _keyReleased(e) {
+        delete this._activeKey[e.which];
+    }
+
+    _keyPressed(e) {
+        this._stopPropagation(e);
+        for (let i = 0; i < this._keyboardCallers.length; this._keyboardCallers[i++](e.which));
+        this._activeKey[e.which] = true;
+    }
+
+    eventPropagation(propagate) {
+        this._stopPropagation = propagate ? function () { } : function (e) { e.preventDefault(); };
+    }
+
+    getActiveKeys() {
+        return this._activeKey;
+    };
+    
+    registerKeypress(func) {
+        this._keyboardCallers.push(func);
+        return (this._keyboardCallers.length - 1);
+    };
+    
+    unregisterKeypress(callerId) {
+        this._keyboardCallers.splice(callerId, 1);
+    }
+
+    onStart() {
+        const self = this;
+
+		document.addEventListener("keydown", function(e) { self._keyPressed(e); }, false);
+		document.addEventListener("keyup", function(e) { self._keyReleased(e); }, false);
+	}
+}
---
+++ src/touchIO.js
@@ -0,0 +1,41 @@
+class TouchIO {
+
+    _name = "TouchIO";
+    _touchCounter = 0;
+    _touchCallers = { };
+
+    constructor() { }
+
+    _getCoordinates(e) {
+        return { 
+            x: e.changedTouches[0].clientX, 
+            y: e.changedTouches[0].clientY
+        };
+    }
+
+    _touchEnd(e) {
+        const currentTouch = this._getCoordinates(e);
+
+        for (let p in this._touchCallers) {
+            if (this._touchCallers.hasOwnProperty(p)) {
+                this._touchCallers[p](currentTouch);
+            }
+        }
+    }
+
+    registerTouch(func) {
+        this._touchCallers[this._touchCounter] = func;
+        this._touchCounter++;
+        return (this._touchCounter - 1);
+    }
+    
+    unregisterTouch(callerId) {
+        delete this._touchCallers[callerId];
+    }
+
+    onStart() {
+        const self = this;
+        
+        document.getElementById(this._gfwk.settings.canvas).addEventListener("touchend", function(e) { self._touchEnd(e); }, false);
+	}
+}
