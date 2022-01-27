2cf4668345ec8a3ee78f1d2bdda2f38c65a3e51b
Matias Iacono <matias.iacono@gmail.com>
2022-01-27 17:03:00 +1300

    Clock.js added

---
+++ src/clock.js
@@ -0,0 +1,28 @@
+class Clock {
+
+    _name = "Clock";
+
+    _counter = 0;
+    triggerTime = 0;
+    _action = { };
+
+    constructor(process) { 
+        if (process !== undefined) {
+			this.setAction(process.action || this._action);
+			this.triggerTime = process.triggerTime || this.triggerTime;
+		}
+    }
+
+    reset() { this._counter = 0; }
+
+    setAction(action) { this._action = action; }
+
+    tick(delta) {
+		this._counter += delta;
+
+		if (this._counter >= this.triggerTime) {
+			this._counter = 0;
+			this._action();
+		}
+	}
+}
