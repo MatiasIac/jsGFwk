679270e5c6f4232dc0cdb9771fd624ccfe5763d8
Matias Iacono <matias.iacono@gmail.com>
2026-01-28 18:47:05 +1300

    Added key pressed functionality

--- src/keyboardIO.js
+++ src/keyboardIO.js
@@ -2,26 +2,60 @@ class KeyboardIO {
 
     _name = "KeyboardIO";
     _keyboardCallers = [];
+    _keyboardPressedCallers = [];
     _activeKey = [];
     _keyDownHandler = null;
     _keyUpHandler = null;
 
     static KEYS = {
         "A": 65,
+        "B": 66,
+        "C": 67,
         "D": 68,
+        "E": 69,
+        "F": 70,
+        "G": 71,
+        "H": 72,
         "I": 73,
         "J": 74,
         "K": 75,
         "L": 76,
-        "N": 78,
-        "S": 83,
-        "W": 87,
         "M": 77,
         "N": 78,
-        "C": 67,
+        "O": 79,
+        "P": 80,
+        "Q": 81,
+        "R": 82,
+        "S": 83,
+        "T": 84,
+        "U": 85,
+        "V": 86,
+        "W": 87,
+        "X": 88,
+        "Y": 89,
+        "Z": 90,
         "ONE": 49,
         "TWO": 50,
         "THREE": 51,
+        "FOUR": 52,
+        "FIVE": 53,
+        "SIX": 54,
+        "SEVEN": 55,
+        "EIGHT": 56,
+        "NINE": 57,
+        "ZERO": 48,
+        "F1": 112,
+        "F2": 113,
+        "F3": 114,
+        "F4": 115,
+        "F5": 116,
+        "F6": 117,
+        "F7": 118,
+        "F8": 119,
+        "F9": 120,
+        "F10": 121,
+        "F11": 122,
+        "F12": 123,
         "SHIFT": 16,
         "SPACEBAR": 32,
         "ENTER": 13,
@@ -33,7 +67,10 @@ class KeyboardIO {
     _stopPropagation() { }
 
     _keyReleased(e) {
+        const wasActive = this._activeKey[e.which] === true;
         delete this._activeKey[e.which];
+        if (!wasActive) { return; }
+        for (let i = 0; i < this._keyboardPressedCallers.length; this._keyboardPressedCallers[i++](e.which));
     }
 
     _keyPressed(e) {
@@ -57,6 +94,15 @@ class KeyboardIO {
     
     unregisterKeypress(callerId) {
         this._keyboardCallers.splice(callerId, 1);
+    }
+
+    registerKeyPressed(func) {
+        this._keyboardPressedCallers.push(func);
+        return (this._keyboardPressedCallers.length - 1);
+    };
+    
+    unregisterKeyPressed(callerId) {
+        this._keyboardPressedCallers.splice(callerId, 1);
     }
 
     onStart() {
