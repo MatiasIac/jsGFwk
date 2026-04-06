f65b0b8c5ab2a49dbe4a9993f2a13d7d5a5507dd
Matias Iacono <matias.iacono@gmail.com>
2026-04-06 12:41:04 +1200

    Updated animator2D.js

--- src/animator2d.js
+++ src/animator2d.js
@@ -6,13 +6,22 @@ class Animator2D {
 	_2Dbuffer = {};
 	_bufferCanvas = {};
 	_lastFrame = 0;
+    _isRunning = false;
+    _boundPointer = null;
+    _maxDelta = 0.1;
+    _now = null;
 
     constructor() {
+        this._now = (typeof performance !== "undefined" && performance.now)
+            ? performance.now.bind(performance)
+            : Date.now;
     }
 
     _pointer() {
-        const thisFrame = new Date().getTime();
-        const delta = (thisFrame - this._lastFrame) / 1000;
+        if (!this._isRunning) { return; }
+        const thisFrame = this._now();
+        const rawDelta = (thisFrame - this._lastFrame) / 1000;
+        const delta = rawDelta > this._maxDelta ? this._maxDelta : rawDelta;
 
         this._lastFrame = thisFrame;
         
@@ -26,7 +35,9 @@ class Animator2D {
             this._gfwk._includes[i].preRender !== undefined && this._gfwk._includes[i].preRender(this._2Dbuffer);
         }
 
+        const postRenderQueue = [];
         for (let name in this._gfwk._gameObjects) {
+            if (!Object.prototype.hasOwnProperty.call(this._gfwk._gameObjects, name)) { continue; }
             if (this._gfwk._gameObjects[name] !== null) {
                 const o = this._gfwk._gameObjects[name];
 
@@ -37,20 +48,25 @@ class Animator2D {
                         o.draw(this._2Dbuffer);
                     this._2Dbuffer.restore();
                 }
+
+                if (o !== undefined && o.postRender !== undefined) {
+                    postRenderQueue.push(o);
+                }
             }
         }
         
         //execute any object that includes a post render
-        for (let name in this._gfwk._gameObjects) {
-            this._gfwk._gameObjects[name].postRender !== undefined && this._gfwk._gameObjects[name].postRender(this._2Dbuffer);
+        for (let i = 0; i < postRenderQueue.length; i++) {
+            postRenderQueue[i].postRender(this._2Dbuffer);
         }
         
         this._2Dcontext.drawImage(this._bufferCanvas, 0, 0);
         
-        requestAnimFrame(this._pointer.bind(this));
+        requestAnimFrame(this._boundPointer);
     }
 
     onStart() {
+        if (this._isRunning) { return; }
 		this._canvas = document.getElementById(this._gfwk.settings.canvas);
 		this._2Dcontext = this._canvas.getContext("2d");
 		this._bufferCanvas = document.createElement('canvas');
@@ -69,8 +85,17 @@ class Animator2D {
                 };
 		})();
 	
-		requestAnimFrame(this._pointer.bind(this));
+        this._isRunning = true;
+        this._lastFrame = this._now();
+        if (this._boundPointer === null) {
+            this._boundPointer = this._pointer.bind(this);
+        }
+		requestAnimFrame(this._boundPointer);
 	}
+
+    onStop() {
+        this._isRunning = false;
+    }
 
 }
 
